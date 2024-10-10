64ab30d97ef32baa91f5f2171008e9273c204202
EdsonML <163768557+EdsonML@users.noreply.github.com>
2024-10-10 17:06:57 -0300

    .

---
+++ assets/perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'Programação', 
+    'O que é Phyton?',
+    'O Phyton é uma linguagem de programação',
+)
+   
+criaCartao(
+    'Geografia',
+    'Qual a capital do Paraná?',
+    'A capital do Paraná é Curitiba',
+)
+    
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'A função é um bloco de código que executa alguma função',
+)
+

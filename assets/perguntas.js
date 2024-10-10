c40ab57f83548759a8929d00a772e72334b20e26
EdsonML <163768557+EdsonML@users.noreply.github.com>
2024-10-10 16:01:42 -0300

    .

---
+++ assets/perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Programação', 
+    'O que é javaScript?',
+    'O javaScript é uma linguagem de programação',
+)
+   
+criaCartao(
+    'Geografia',
+    'Qual a capital do Paraná?',
+    'A capital do Paraná é Curitiba',
+)
+    
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'A função é um bloco de código que executa uma função',
+)
+}
+

c79ebdce7d3b886e3ab2e2bdfd623fc1ce075b42
EdsonML <163768557+EdsonML@users.noreply.github.com>
2024-10-17 22:07:25 -0300

    .

--- assets/perguntas.js
+++ assets/perguntas.js
@@ -1,18 +1,40 @@
 criaCartao(
-    'Programação', 
-    'O que é Phyton?',
-    'O Phyton é uma linguagem de programação',
+    'GITHUB', 
+    'O que é GITHUB?',
+    'GitHub é uma plataforma de desenvolvimento colaborativo que aloja projetos na nuvem',
 )
    
 criaCartao(
-    'Geografia',
-    'Qual a capital do Paraná?',
-    'A capital do Paraná é Curitiba',
+    'QUÍMICA',
+    'O que é calor?',
+    'Calor é o fluxo de energia que flui do corpo mais quente em direção ao corpo mais frio',
 )
     
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'A função é um bloco de código que executa alguma função',
+    'ALGORITMO',
+    'O que é algoritmo?',
+    'Conjunto das regras e procedimentos lógicos perfeitamente definidos que levam à solução de um problema em um número finito de etapas',
 )
 
+criaCartao(
+    'SOBRE A IMPORTÂNCIA DA MATEMÁTICA',
+    'Qual é a importância da Matemática?',
+    'A matemática é de grande importância para todos importante para o crescimento e desenvolvimento da humanidade',
+) 
+
+criaCartao(
+    'PROGRAMAÇÃO',
+    'O que é javaScript?',
+    'O JavaScript é uma linguagem de programação',
+)
+
+criaCartao(
+    'GEOGRAFIA',
+    'Qual a capital do Paraná é Curitiba',
+)
+
+criaCartao(
+    'FÍSICA',
+    ' O que é inércia?',
+    'Resistência que a matéria oferece à aceleração',
+)

4205d2a65685c1f8804f2d022f320503b064d6d1
EdsonML <163768557+EdsonML@users.noreply.github.com>
2024-10-23 16:17:26 -0300

    .

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ function criaCartao(categoria, pergunta, resposta) {
    let cartao = document.createElement ('article')
    cartao.className = 'cartao'
 
-    cartao.innerHTML =`
-     <div class="cartao__conteudo">
-     <h3>${categoria}</h3>
+cartao.innerHTML =`
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
     <p>${pergunta}</p>
      </div>
@@ -14,5 +14,14 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     </div>
  `
+
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle ('active, respostaEstavisivel')
+ }
+
+ cartao.addEventListener( 'click', viraCartao)
  container.appendChild(cartao)
 }

8cdf0a38560248d042e3b00ce8a86ea9fcc29e0d
EdsonML <163768557+EdsonML@users.noreply.github.com>
2024-10-23 16:36:27 -0300

    .

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ cartao.innerHTML =`
         cartao.classList.toggle ('active',respostaEstaVisivel)
  }
 
- cartao.addEventListener('click', viraCartao)
+ cartao.addEventListener('click', (viraCartao))
 
  container.appendChild(cartao)
 }

af7e886779739cf609a79881949caa56af5b6edf
EdsonML <163768557+EdsonML@users.noreply.github.com>
2024-10-23 15:30:18 -0300

    .

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 function criaCartao(categoria, pergunta, resposta) {
-    console.log(categoria, pergunta, resposta);
+   let container = document.getElementById ('container')
+   let cartao = document.creatElement ('article')
+   cartao.classname = 'cartao'
+
+    cartao.innerHTML =`
+     <div class="cartao__conteudo">
+     <h3>GITHUB</h3>
+    <div class="cartao__conteudo__pergunta">
+    <p>O que é GITHUB?</p>
+     </div>
+    <div class="cartao__conteudo__resposta">
+    <p>O GitHub é uma plataforma de desenvolvimento colaborativo que aloja projetos na nuvem</p>
+    </div>
+    </div>
+ `
+ container.appendChild( 'cartao')
 }
